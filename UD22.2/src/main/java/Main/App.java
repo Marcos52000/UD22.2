9319e1033f32bafa4ec7b803820ad7099bb71d29
Marcos <marcoscousomartin@gmail.com>
2022-02-25 21:33:19 +0100

    Commit

---
+++ UD22.2/src/main/java/Main/App.java
@@ -0,0 +1,18 @@
+package Main;
+
+import Controller.Controlador;
+import Model.Modelo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Controlador.setdatos();
+    	Controlador.ventanaseleccion();
+		
+    }
+}
